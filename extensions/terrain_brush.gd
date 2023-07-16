4869d13b0ec79c5be2d781d6f95e817076326bda
Christopher Dharma <42819852+cdharma3@users.noreply.github.com>
2023-07-16 00:27:03 -0700

    Adding new biome popup

--- extensions/terrain_brush.gd
+++ extensions/terrain_brush.gd
@@ -47,32 +47,42 @@ func start():
 
     # Setup terrain preset buttons
     var new_button = terrain_brush.CreateButton("New Biome", Global.Root + "icons/add.png")
-    new_button.connect("pressed", self, "new_biome")
+    new_button.connect("pressed", self, "new_biome_window")
     terrain_brush.Align.move_child(new_button, 6)
 
-    var save_button = terrain_brush.CreateButton("Save Biome", "res://ui/icons/menu/save.png")
+    var save_button = terrain_brush.CreateButton("Save Biomes", "res://ui/icons/menu/save.png")
     save_button.connect("pressed", self, "save_biomes")
     save_button.hint_tooltip = "WARNING: This will overwrite your current preset file"
     terrain_brush.Align.move_child(save_button, 7)
 
-    var load_button = terrain_brush.CreateButton("Load Biome", "res://ui/icons/menu/redo.png")
+    var load_button = terrain_brush.CreateButton("Reload Biomes", "res://ui/icons/menu/redo.png")
     load_button.connect("pressed", self, "load_biomes")
     terrain_brush.Align.move_child(load_button, 8)
 
+    var biome_window = load(Global.Root + "scenes/NewBiomeWindow.tscn").instance()
+    biome_window.name = "NewBiomeWindow"
+    biome_window.get_node("Margins/VAlign/Buttons/OkayButton").connect("pressed", self, "add_biome", [biome_window])
+    Global.Editor.get_node("Windows").add_child(biome_window, true)
     
 # Utilities
-func sync_biome():
-    ## Sync currently set biome with terrain list
-    var cur_biome = biomes.keys()[biome_dropdown.selected]
-    log_info("Updating biome " + cur_biome)
+func new_biome_window():
+    # Popup new biome dialog
+    var biome_menu =  Global.Editor.get_node("Windows/NewBiomeWindow")
+    biome_menu.popup_centered()
     
-    for i in range(0, terrain_list.get_item_count()):
-        biomes[cur_biome][i] = Global.World.Level.Terrain.GetTexture(i).resource_path
-    
-func new_biome():
-    ## Add a new biome to the biomes dictionary
-    # TODO: Add ability to customize new biome name
-    biomes["new_biome"] = [
+func add_biome(biome_window):
+    # Hide popup
+    biome_window.hide()
+
+    # Add new biome to biomes dictionary
+    # TODO: Add warning dialog if overwriting existing biome
+    var biome_name = biome_window.get_node("Margins/VAlign/Label/LabelLineEdit").text
+    if not biome_name:
+        log_warn("Invalid or empty biome name!")
+        return 
+
+    log_info("Adding new biome " + biome_name)
+    biomes[biome_name] = [
         "res://textures/terrain/terrain_dirt.png", 
         "res://textures/terrain/terrain_dirt.png", 
         "res://textures/terrain/terrain_dirt.png", 
@@ -80,8 +90,14 @@ func new_biome():
     ]
 
     update_biomes()
+    set_biome(-1)
 
-func save_biomes(): 
+func save_biomes():
+    # TODO: Allow user to save to file
+
+    # Sync current biome first 
+    sync_biome()
+
     # Save current biomes state to preset file
     log_info("Saving file " + BIOMES_PATH + "...")
     var file = File.new()
@@ -105,6 +121,7 @@ func load_biomes() -> int:
 
     # Override biome dropdown to link into script
     update_biomes()
+    set_biome(0)
     return 0
 
 func update_biomes():
@@ -112,10 +129,11 @@ func update_biomes():
     biome_dropdown.clear()
     for biome in biomes.keys():
         biome_dropdown.add_item(biome)
-    set_biome(0)
 
 func set_biome(index):
     log_info("Setting biome to " + biomes.keys()[index])
+    biome_dropdown.select(index)
+    biome_dropdown.update()
 
     var textures = biomes[biomes.keys()[index]]
     for i in range(0, len(textures)):
@@ -124,7 +142,14 @@ func set_biome(index):
             Global.World.Level.Terrain.SetTexture(texture, i)
             terrain_list.set_item_icon(i, texture)
             terrain_list.set_item_text(i, parse_resource_name(texture))
+
+func sync_biome():
+    ## Sync currently set biome with terrain list
+    var cur_biome = biomes.keys()[biome_dropdown.selected]
+    log_info("Updating biome " + cur_biome)
     
+    for i in range(0, terrain_list.get_item_count()):
+        biomes[cur_biome][i] = Global.World.Level.Terrain.GetTexture(i).resource_path    
 
 func load_texture(texture_path):
     log_info("Loading texture " + texture_path)
@@ -146,10 +171,10 @@ func parse_resource_name(resource) -> String:
 
 func popup_terrain_window(index):
     ## HACK: Wrapper function around the open terrain window buttons
-    ## Currently it seems like the TerrainWindow is using the show
-    ## method, which on hide does **not** emit the popup_hide signal.
-    ## Thus this wrapper forces the window to 'Open' briefly, hides it
-    ## immediately then popup immediately after
+    ## Currently it seems like the TerrainWindow is not properly emitting
+    ## the popup_hide signal. 
+    ## Thus this wrapper forces the window to 'Open' briefly, 
+    ## hides it immediately, then popup immediately after to force the signal to trigger
     var terrain_window = Global.Editor.TerrainWindow
     terrain_window.Open(index)
     terrain_window.hide()
