f8cb3b5b59655f41aa9a7736f263e1348ab45fa5
Christopher Dharma <42819852+cdharma3@users.noreply.github.com>
2023-08-01 01:11:28 -0700

    Some small 8 terrain compatiblity

--- scripts/terrain_brush.gd
+++ scripts/terrain_brush.gd
@@ -1,15 +1,14 @@
 var script_class = "tool"
 
-# Globals
-var terrain_brush
-var terrain_list
-var terrain_buttons
-var biome_dropdown
-
 # Script constants
 # TODO: Add support for multiple biome presets in the future
-const BIOMES_PATH: String = "user://preset1.dungeondraft_biomes"
+const BIOMES_PATH: String = "user://default.dungeondraft_biomes"
 const DEFAULT_BIOMES: String = "res://data/default.dungeondraft_biomes"
+# HACK: Would probably be better to save this stuff in the map files themselves,
+# but the risk of incompatibilty is... extreme to say the least
+
+# TODO: Move this into another class
+const USER_DATA: String = "user://terrain_presets.json"
 
 # Script verbosity mode:
 # INFO = All logging messages printed
@@ -21,7 +20,14 @@ enum VERBOSITY {
     ERROR
 }
 
-const DEBUG_MODE = VERBOSITY.WARN
+const DEBUG_MODE = VERBOSITY.INFO
+
+# Predefined Globals
+var terrain_brush
+var terrain_list
+var terrain_buttons
+var biome_dropdown
+var expand_slots
 
 func init_globals():
     # WARNING: Call at the start of script and ONLY ONCE
@@ -30,10 +36,14 @@ func init_globals():
     terrain_list = terrain_brush.Align.get_child(7).get_child(0)
     terrain_buttons = terrain_brush.Align.get_child(7).get_child(1).get_children()
     biome_dropdown = terrain_brush.Align.get_child(6)
+    expand_slots = terrain_brush.Align.get_child(10)
 
-
+# Script Globals
 # Biome dictionary
 var biomes: Dictionary
+
+# User settings dictionary
+var user_data: Dictionary
 
 # Script entry point
 func start():
@@ -55,12 +65,21 @@ func start():
         conns = terrain_buttons[i].get_signal_connection_list("pressed")
         terrain_buttons[i].disconnect("pressed", conns[0].target, conns[0].method)
         terrain_buttons[i].connect("pressed", self, "popup_terrain_window", [i])
+    
+    # HACK: Overriding the 'pressed' signal for the expand_slots button
+    conns = expand_slots.get_signal_connection_list("toggled")
+    expand_slots.disconnect("toggled", conns[0].target, conns[0].method)
+    expand_slots.connect("toggled", self, "expand")
 
     # HACK: Disconnecting undefined method from 'about_to_show' signal, this 
     # was throwing an error message, and I'm not entirely sure why :/
     Global.Editor.TerrainWindow.disconnect("about_to_show", Global.Editor.TerrainWindow, "_on_TerrainWindow_about_to_show")
-    
+
     Global.Editor.TerrainWindow.connect("popup_hide", self, "sync_biome")
+
+    # Load user data
+    if load_user_data() != 0:
+        log_err("Some sort of error when loading user data!")
 
     # Load biomes into biomes dictionary, then override
     # the default biomes terrain set with our own
@@ -94,7 +113,8 @@ func start():
     Global.Editor.get_node("Windows").add_child(biome_window, true)
     
     
-# Utilities    
+# Utilities
+  
 func add_biome(biome_window):
     # Hide popup
     biome_window.hide()
@@ -104,7 +124,7 @@ func add_biome(biome_window):
     var biome_name = biome_window.get_node("Margins/VAlign/Label/LabelLineEdit").text
     if not biome_name or len(biome_name) <= 0:
         log_warn("Invalid or empty biome name!")
-        Editor.Warn("Warning!", "Invalid or empty biome name!")
+        Global.Editor.Warn("Warning!", "Invalid or empty biome name!")
         return 
 
     log_info("Adding new biome " + biome_name)
@@ -118,17 +138,17 @@ func add_biome(biome_window):
     ]
 
     update_biomes()
-    set_biome(0)
+    set_biome(biome_dropdown.get_item_count() - 1)
 
 func del_biome():
     # If biome is the last biome in the dict, lets not delete it eh?
     if biomes.keys().size() <= 1:
         log_warn("Must have at least 1 biome!")
-        Editor.Warn("Warning!", "Must have at least 1 biome!")
+        Global.Editor.Warn("Warning!", "Must have at least 1 biome!")
         return
         
     # Get currently selected biome
-    var cur_biome = biomes.keys()[biome_dropdown.selected]
+    var cur_biome = biome_dropdown.text
 
     # Remove biome from biomes dict
     if not biomes.erase(cur_biome):
@@ -137,15 +157,20 @@ func del_biome():
     update_biomes()
     set_biome(0)
 
+# Save current biomes state to preset file
 func save_biomes():
-    # Save current biomes state to preset file
-    # HACK: Slight hack, guessing this is the proper confirm node?
-    var save_confirm = Global.Editor.get_node("Windows/Confirm")
-    save_confirm.dialog_text = "WARNING: This will overwrite your current biome preset, are you sure?"
-    if (!save_confirm.get_ok().is_connected("pressed", self, "_save_confirmed")):
-        save_confirm.get_ok().connect("pressed", self, "_save_confirmed")
-        save_confirm.get_cancel().connect("pressed", self, "_save_cancelled")
-    save_confirm.popup_centered_clamped()
+    # Prompt for confirmation if file already exists
+    var file = File.new()
+    if file.file_exists(BIOMES_PATH):
+        # HACK: Slight hack, guessing this is the proper confirm node?
+        var save_confirm = Global.Editor.get_node("Windows/Confirm")
+        save_confirm.dialog_text = "WARNING: This will overwrite your current biome preset, are you sure?"
+        if (!save_confirm.get_ok().is_connected("pressed", self, "_save_confirmed")):
+            save_confirm.get_ok().connect("pressed", self, "_save_confirmed")
+            save_confirm.get_cancel().connect("pressed", self, "_save_cancelled")
+        save_confirm.popup_centered_clamped()
+    else: 
+        _save_confirmed()
     
 
 func _save_confirmed():
@@ -158,8 +183,33 @@ func _save_confirmed():
 func _save_cancelled():
     log_info("Save cancelled")
 
+func load_user_data() -> int:
+    log_info("Loading user data...")
+
+    # Populate user data dictionaries
+    var file = File.new()
+    file.open(USER_DATA, File.READ)
+    var line = file.get_as_text()
+    user_data = JSON.parse(line).result
+    file.close()
+
+    if not user_data:
+        log_warn("Failed to load user data " + USER_DATA + ", file is either missing or corrupted")
+        log_info("Initializing default user data dictionary... at " + USER_DATA)
+        file = File.new()
+        file.open(USER_DATA, File.WRITE)
+        # TODO: Move this out of here, temporary location
+        user_data = {"maps": {}}
+        file.store_line(JSON.print(user_data, "\t"))
+        file.close()
+
+    return 0
+
 func load_biomes() -> int:
     log_info("Loading biomes...")
+
+    # Save last biome selected
+    var last_biome = biome_dropdown.text
 
     # Populate biomes
     var file = File.new()   
@@ -176,10 +226,15 @@ func load_biomes() -> int:
         biomes = JSON.parse(line).result
         file.close()
         save_biomes()
-
-    # Override biome dropdown to link into script
+        
     update_biomes()
-    set_biome(0)
+
+    # If the last selected biome still exists, select it
+    var last_biome_idx = biomes.keys().find(last_biome)
+    if last_biome_idx != -1:
+        set_biome(last_biome_idx)
+    else: 
+        set_biome(0)
     return 0 
 
 func update_biomes():
@@ -188,20 +243,45 @@ func update_biomes():
     for biome in biomes.keys():
         biome_dropdown.add_item(biome)
 
+func expand(value):
+    log_info("Expanded slots button pressed")
+    # Ensure this is called before updating the buttons
+    terrain_brush.Tool.ExpandSlots(value)
+
+    # TODO: Is there a better way to do this?
+    # Currently just getting the newly created buttons, then overriding them
+    terrain_buttons = terrain_brush.Align.get_child(7).get_child(1).get_children()
+    for i in range(0, len(terrain_buttons)):
+        var conns = terrain_buttons[i].get_signal_connection_list("pressed")
+        terrain_buttons[i].disconnect("pressed", conns[0].target, conns[0].method)
+        terrain_buttons[i].connect("pressed", self, "popup_terrain_window", [i])
+
+    set_biome(biome_dropdown.selected)
+
 func set_biome(index):
-    var biome_name = biomes.keys()[index]
+    var biome_name = biome_dropdown.get_item_text(index)
     log_info("Setting biome to " + biome_name)
-    # TODO: How do I get the dropdown to switch in code??? For some reason
-    # this doesn't work... even after calling update???
-    # Interestingly this function still works if called from the signal... 😠
     biome_dropdown.select(index)
-    biome_dropdown.update()
     
-    var textures = biomes[biome_name]
     # Only pop up accept dialog once
     var clean = true
-    for i in range(0, len(textures)):
-        var texture = load_texture(textures[i])
+    var len_textures = 4
+
+    # If terrain list has been expanded, lazily attempt to load expanded preset
+    if (Global.World.Level.Terrain.ExpandedSlots):
+        len_textures = 8
+        if len(biomes[biome_name]) < 8:
+            log_info("Appending default terrain")
+            biomes[biome_name] += [
+                "res://textures/terrain/terrain_dirt.png", 
+                "res://textures/terrain/terrain_dirt.png", 
+                "res://textures/terrain/terrain_dirt.png", 
+                "res://textures/terrain/terrain_dirt.png"
+            ]
+    
+    # Load textures
+    for i in range(0, len_textures):
+        var texture = load_texture(biomes[biome_name][i])
         if texture:
             Global.World.Level.Terrain.SetTexture(texture, i)
             terrain_list.set_item_icon(i, texture)
@@ -209,16 +289,23 @@ func set_biome(index):
         else:
             clean = false
     if not clean:
-        Editor.Warn("Alert!", "Some kind of error when attempting to load biome " + biome_name 
+        log_err("Some kind of error when attempting to load biome " + biome_name 
+        + ".\nPlease check that all asset packs are properly loaded!")
+
+        Global.Editor.Warn("Alert!", "Some kind of error when attempting to load biome " + biome_name 
         + ".\nPlease check that all asset packs are properly loaded!")
 
 
 func sync_biome():
-    ## Sync currently set biome with terrain list
-    var cur_biome = biomes.keys()[biome_dropdown.selected]
+    # Sync currently set biome with terrain list
+    var cur_biome = biome_dropdown.text
     log_info("Updating biome " + cur_biome)
-    
-    for i in range(0, terrain_list.get_item_count()):
+
+    var len_terrains = 4
+    if Global.World.Level.Terrain.ExpandedSlots:
+        len_terrains = 8
+
+    for i in range(0, len_terrains):
         biomes[cur_biome][i] = Global.World.Level.Terrain.GetTexture(i).resource_path    
 
 func load_texture(texture_path):
@@ -257,6 +344,7 @@ func popup_terrain_window(index):
     # to set the grid menu thing...
     # Thus this wrapper forces the window to 'Open' briefly, 
     # hides it immediately, then popup immediately after to force the signal to trigger
+    log_info("Terrain window popped up")
     var terrain_window = Global.Editor.TerrainWindow
     terrain_window.Open(index)
     terrain_window.hide()
